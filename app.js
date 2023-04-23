220d74bc56a30ad177e70aaf770df9ce02a19b21
anmaiden <aannna123@gmail.com>
2023-04-23 13:41:31 +0300

    fix(basic-3.4): remove tag selectors

--- app.js
+++ app.js
@@ -33,17 +33,17 @@ var createNewTaskElement=function(taskString){
   var deleteButtonImg=document.createElement("img");//delete button image
 
   label.innerText=taskString;
-  label.className='task';
+  label.className='task app-label';
 
   //Each elements, needs appending
   checkBox.type="checkbox";
   editInput.type="text";
-  editInput.className="task";
-
+  editInput.className="task task-input";
+  listItem.className="task-list__li";
   editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-  editButton.className="edit";
+  editButton.className="edit app-btn";
 
-  deleteButton.className="delete";
+  deleteButton.className="delete app-btn";
   deleteButtonImg.src='./remove.svg';
   deleteButton.appendChild(deleteButtonImg);
 
